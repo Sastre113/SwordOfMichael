5a1f1d91a6502018de934bbe0534f84a4e9c4c90
Miguel Á. Sastre <sastre113@gmail.com>
2022-10-04 21:21:37 +0200

    initial commmit

---
+++ src/main/java/Sword/Of/Michael/SwordOfMichaelApplication.java
@@ -0,0 +1,13 @@
+package Sword.Of.Michael;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwordOfMichaelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SwordOfMichaelApplication.class, args);
+	}
+
+}
---
+++ src/test/java/Sword/Of/Michael/SwordOfMichaelApplicationTests.java
@@ -0,0 +1,13 @@
+package Sword.Of.Michael;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SwordOfMichaelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
